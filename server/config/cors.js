9069279b6d3f5e443b7abff0521ca018e8374369
akshayaav246-collab <akshayaav246@gmail.com>
2026-02-26 14:47:06 +0530

    cors.js file updated

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,14 +1,11 @@
 // Dynamically reflect any origin to allow all ports/hosts securely
 const corsOptions = {
     origin: "*",
-    credentials: false,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+    credentials: false
 };
 
 const socketCorsOptions = {
     origin: "*", // allow all for socket
-    methods: ['GET', 'POST', 'OPTIONS']
 };
 
 
